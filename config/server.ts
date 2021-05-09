0c344c28badf3aa87bca75654187543af26b9901
quanghieple <quanghiep.lee@gmail.com>
2021-05-09 10:58:01 +0700

    fix get react_app_env

--- config/server.ts
+++ config/server.ts
@@ -2,9 +2,4 @@ import devConfig from "./dev";
 import prodConfig from "./prod";
 import stagConfig from "./stag";
 
-const { REACT_APP_ENV } = process.env;
-
-const ENV = REACT_APP_ENV || 'dev'
-
-console.log("hieplq", ENV)
-export const server = (ENV === 'dev' ? devConfig : (ENV === 'staging' ? stagConfig : prodConfig))
+export const server = (REACT_APP_ENV === 'dev' ? devConfig : (REACT_APP_ENV === 'staging' ? stagConfig : prodConfig))
