c58714c2e2e2e319190cd69a8b850aa1635e6863
Hiep Le <hieplq@vingroup.net>
2021-09-21 23:30:52 +0700

    add

---
+++ config/firebase.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBEAtCzQXZu26Uhu3wrMFTN0kIsif_qtZk",
+  authDomain: "hr-sol-289314.firebaseapp.com",
+  databaseURL: "https://hr-sol-289314.firebaseio.com",
+  projectId: "hr-sol-289314",
+  storageBucket: "hr-sol-289314.appspot.com",
+  messagingSenderId: "353192528519",
+  appId: "1:353192528519:web:f556642adb5cc681fb4622"
+}
